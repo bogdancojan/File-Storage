cb31e4b44a0f0e46f14bea908db78685c773f0ab
bogdancojan <Bogdan.Cojan@theaccessgroup.com>
2024-02-01 01:06:57 +0200

    Added validation for file size in Document model

--- app/models/document.rb
+++ app/models/document.rb
@@ -2,4 +2,13 @@ class Document < ApplicationRecord
   has_one_attached :file
 
   validates :file, presence: true
+  validate :file_size_under_5_mb
+
+  private
+
+  def file_size_under_5_mb
+    if file.attached? && file.blob.byte_size > 5.megabytes
+      errors.add(:file, "#{file.blob.filename} is too large (maximum is 5 MB)")
+    end
+  end
 end
--- spec/models/document_spec.rb
+++ spec/models/document_spec.rb
@@ -1,6 +1,19 @@
 require 'rails_helper'
 
 RSpec.describe Document, type: :model do
+  let(:small_file) { Rack::Test::UploadedFile.new('spec/fixtures/small_file.txt', 'text/plain') }
+  let(:large_file) { Rack::Test::UploadedFile.new('spec/fixtures/large_file.txt', 'text/plain') }
+
+  before do
+    File.write('spec/fixtures/small_file.txt', 'a' * 1.megabyte)
+    File.write('spec/fixtures/large_file.txt', 'a' * 6.megabytes)
+  end
+
+  after do
+    File.delete('spec/fixtures/small_file.txt')
+    File.delete('spec/fixtures/large_file.txt')
+  end
+
   it "is valid with a file attached" do
     document = Document.new
     document.file.attach(io: File.open('spec/fixtures/test_file.txt'), filename: 'test_file.txt', content_type: 'application/txt')
@@ -10,5 +23,14 @@ RSpec.describe Document, type: :model do
   it "is not valid without a file attached" do
     document = Document.new
     expect(document).to_not be_valid
+  end
+
+  it 'validates that the file size is under 5 MB' do
+    document = Document.new(file: small_file)
+    expect(document).to be_valid
+
+    document = Document.new(file: large_file)
+    expect(document).not_to be_valid
+    expect(document.errors.full_messages).to include("File large_file.txt is too large (maximum is 5 MB)")
   end
 end
